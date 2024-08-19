b3b6c35d549e2d974487e319704403d3260233a6
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-08-20 01:47:14 +0530

    backend url added

--- environmentVariables.js
+++ environmentVariables.js
@@ -1,3 +1,3 @@
-const API='https://my-portfolio-backend-blond-iota.vercel.app';
+const API='https://my-portfolio-backend-hazel.vercel.app';
 // const API='http://localhost:8000'
 export default API;
